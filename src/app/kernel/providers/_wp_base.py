69fdd51bcfed58876ff27f972d9b044e48a252d4
Raisess <raisechif@gmail.com>
2023-09-08 16:15:18 -0300

    refactor: use a common provider base for wp based sites

---
+++ src/app/kernel/providers/_wp_base.py
@@ -0,0 +1,34 @@
+"""
+- This class can handle sites with the h2-entry-title
+  list pattern and h1-entry-title page pattern, I assume
+  they're made using wordpress, so we can have only one
+  class to do the job.
+"""
+
+from urllib import parse
+
+from app.kernel.provider import Provider
+from app.models import TitleModel
+
+class _WPBase(Provider):
+  def __init__(self, host: str):
+    super().__init__(host)
+
+  def search(self, term: str) -> list[str]:
+    data = self._fetch(f"?s={parse.quote(term)}")
+    result = data.find_all("h2", attrs={ "class": "entry-title" })
+    urls = [item.find("a").get("href") for item in result]
+    ids = [item.split("/")[-2:][0] for item in urls]
+    return ids
+
+  def get(self, id: str) -> list[TitleModel]:
+    data = self._fetch(id)
+    name = data.find("h1", attrs={ "class": "entry-title" }).get_text()
+    anchors = data.find_all("a")
+    result = []
+    for anchor in anchors:
+      link = anchor.get("href")
+      if link and link.startswith("magnet:?"):
+        result.append(link)
+
+    return [TitleModel(name, item) for item in result]
--- src/app/kernel/providers/comando_to.py
+++ src/app/kernel/providers/comando_to.py
@@ -1,27 +1,5 @@
-from urllib import parse
+from app.kernel.providers._wp_base import _WPBase
 
-from app.kernel.provider import Provider
-from app.models import TitleModel
-
-class ComandoTo(Provider):
+class ComandoTo(_WPBase):
   def __init__(self):
     super().__init__("aHR0cHM6Ly9jb21hbmRvLmxh")
-
-  def search(self, term: str) -> list[str]:
-    data = self._fetch(f"?s={parse.quote(term)}")
-    result = data.find_all("h2", attrs={ "class": "entry-title" })
-    urls = [item.find("a").get("href") for item in result]
-    ids = [item.split("/")[-2:][0] for item in urls]
-    return ids
-
-  def get(self, id: str) -> list[TitleModel]:
-    data = self._fetch(id)
-    name = data.find("h1", attrs={ "class": "entry-title" }).get_text()
-    anchors = data.find_all("a")
-    result = []
-    for anchor in anchors:
-      link = anchor.get("href")
-      if link and link.startswith("magnet:?"):
-        result.append(link)
-
-    return [TitleModel(name, item) for item in result]
